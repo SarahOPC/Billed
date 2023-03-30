db16f07ff4bf07faf4eca9bba11e7355a1c573fe
Sarah Raguin <sarahraguin@gmail.com>
2023-03-30 09:51:12 -0400

    Bug #4 resolved

--- front/src/containers/Dashboard.js
+++ front/src/containers/Dashboard.js
@@ -132,20 +132,22 @@ export default class {
     this.onNavigate(ROUTES_PATH['Dashboard'])
   }
 
+//----------------------------------------Correction bug #4 by commented lines looking if two bills are selected----------------------
+
   handleShowTickets(e, bills, index) {
     if (this.counter === undefined || this.index !== index) this.counter = 0
     if (this.index === undefined || this.index !== index) this.index = index
-    if (this.counter % 2 === 0) {
+    //if (this.counter % 2 === 0) {
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
       $(`#status-bills-container${this.index}`)
         .html(cards(filteredBills(bills, getStatus(this.index))))
       this.counter ++
-    } else {
-      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
-      $(`#status-bills-container${this.index}`)
-        .html("")
-      this.counter ++
-    }
+    //} else {
+    //  $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
+    //  $(`#status-bills-container${this.index}`)
+    //    .html("")
+    //  this.counter ++
+    //}
 
     bills.forEach(bill => {
       $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
