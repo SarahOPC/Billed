cfd30430418b37c79eaed096755b8e56ca12e4e8
Sarah Raguin <sarahraguin@gmail.com>
2023-04-11 13:47:15 -0400

    Integration test Get Bills done

--- front/src/__tests__/Bills.js
+++ front/src/__tests__/Bills.js
@@ -150,3 +150,80 @@ describe("Given I am connected as an employee", () => {
     })
   })
 })
+
+//----------------------------------------Integration test GET----------------------------------------
+
+describe("Given I am a user connected as Employee", () => {
+  describe("When I navigate to Dashboard", () => {
+    test("fetches bills from mock API GET", async () => {
+      localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "e@e" }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.Bills)
+      await waitFor(() => screen.getByText("Mes notes de frais"))
+      const contentTableType  = await screen.getByText("Type")
+      expect(contentTableType).toBeTruthy()
+      const contentTableName  = await screen.getByText("Nom")
+      expect(contentTableName).toBeTruthy()
+      const contentTableDate  = await screen.getByText("Date")
+      expect(contentTableDate).toBeTruthy()
+      const contentTablePrice  = await screen.getByText("Montant")
+      expect(contentTablePrice).toBeTruthy()
+      const contentTableStatus  = await screen.getByText("Statut")
+      expect(contentTableStatus).toBeTruthy()
+      const contentTableActions  = await screen.getByText("Actions")
+      expect(contentTableActions).toBeTruthy()
+      const contentTableBody  = document.getElementById('[data-testid="tbody"]')
+      expect(contentTableBody.childElementCount).toBeGreaterThanOrEqual(1)
+    })
+  describe("When an error occurs on API", () => {
+    beforeEach(() => {
+      jest.spyOn(mockStore, "bills")
+      Object.defineProperty(
+          window,
+          'localStorage',
+          { value: localStorageMock }
+      )
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee',
+        email: "e@e"
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.appendChild(root)
+      router()
+    })
+    test("fetches bills from an API and fails with 404 message error", async () => {
+
+      mockStore.bills.mockImplementationOnce(() => {
+        return {
+          list : () =>  {
+            return Promise.reject(new Error("Erreur 404"))
+          }
+        }})
+      window.onNavigate(ROUTES_PATH.Bills);
+      await new Promise(process.nextTick);
+      const message = await screen.getByText(/Erreur 404/)
+      expect(message).toBeTruthy()
+    })
+
+    test("fetches messages from an API and fails with 500 message error", async () => {
+
+      mockStore.bills.mockImplementationOnce(() => {
+        return {
+          list : () =>  {
+            return Promise.reject(new Error("Erreur 500"))
+          }
+        }})
+
+      window.onNavigate(ROUTES_PATH.Bills);
+      await new Promise(process.nextTick);
+      const message = await screen.getByText(/Erreur 500/)
+      expect(message).toBeTruthy()
+    })
+  })
+
+  })
+})
